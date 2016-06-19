80058ec43f92f8ab461cdc1e1fb9cbf45542ae09
Josh Fox <jjv360@me.com>
2016-06-19 10:14:18 +0200

    Added tests nd gulp build

---
+++ gulpfile.js
@@ -0,0 +1,74 @@
+/**
+ * Gulp build file for js-alert
+ *
+ *	Usage:
+ *
+ *		npm install
+ *		npm run gulp
+ *
+ *
+ *	To watch for changes and rebuild:
+ *
+ *		npm run gulp watch
+ *
+ */
+ 
+// Dependencies
+var gulp = require("gulp");
+var sourcemaps = require("gulp-sourcemaps");
+var browserify = require('browserify');
+var babelify = require('babelify');
+var uglify = require("gulp-uglify");
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+
+
+// Gulp default action, build the project
+gulp.task("default", ["build"]);
+
+// Gulp build action, build the JavaScript and puts the compiled file into ./dist
+gulp.task("build", function() {
+	
+	// Browserify it
+	return browserify({
+		entries: './src/index.js',
+		debug: true,
+		standalone: "JSAlert"
+	})
+	
+	// Do babelify transforms
+	.transform(babelify, {presets: ["es2015"], plugins: ["add-module-exports"]})
+	
+	// Create a single bundle file
+	.bundle()
+	
+	// Set output name
+	.pipe(source('jsalert.min.js'))
+	
+	// Convert to a buffered file object
+	.pipe(buffer())
+	
+	// Start creating the sourcemap file
+	.pipe(sourcemaps.init({loadMaps: true}))
+	
+	// Minify it
+	.pipe(uglify())
+	
+	// Output the sourcemaps
+	.pipe(sourcemaps.write("."))
+	
+	// Output to the dist folder
+	.pipe(gulp.dest("dist"))
+	
+});
+
+// Gulp watch action, watch for changes and rebuild the project each time
+gulp.task("watch", ["default"], function() {
+
+	var watcher = gulp.watch("src/**/*", ["default"]);
+	watcher.on("change", function(event) {
+		console.log("File " + event.type + ": " + event.path);
+	});
+	console.log("Watching for changed files...");
+	
+});
--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@
 
 export default class JSAlert {
 	
+	/** @static Creates and shows a new alert with the specified text */
+	static alert(text, title, closeText = "Close") {
+		
+		// Create alert
+		var alert = new JSAlert();
+		alert.text = text;
+		
+		window.alert(text);
+		
+		return alert.show();
+		
+	}
 	
+	
+	/** Shows the alert. Returns a Promise which is resolved when the alert closes. */
+	show() {
+		
+		return Promise.resolve(true);
+		
+	}
 	
 }
